65f31ea98c232d5c6c00c67eb673a75dd0297056
Zoketa Hznor <Zoketa Hznor@DESKTOP-9M4V2BD>
2025-12-15 12:15:51 +0100

    Add project files.

---
+++ FirstMAUI/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace FirstMAUI
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("centurygothic.ttf", "CenturyGothicRegular");
+                    fonts.AddFont("centurygothic_bold.ttf", "CenturyGothicBold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
